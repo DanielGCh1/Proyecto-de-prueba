094c09cbef7705dce7c13cef0733ae9ea238e82b
DanielGCh1 <dgchaarturo@gmail.com>
2019-12-04 22:52:23 -0600

    Hola desde la Rama_Extra_1_Master

--- ProyectoDePrueba/Program.cs
+++ ProyectoDePrueba/Program.cs
@@ -7,7 +7,8 @@ namespace ProyectoDePrueba
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("En el master");
+            Console.WriteLine("En la Rama_Extra_1_Master");
+            Console.WriteLine("Hola desde la Rama_Extra_1_Master");
         }
     }
 }
